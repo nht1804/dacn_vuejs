6ecded293080aaec95e857761905419563e4e7ff
nht1804 <nhthuong1804@gmail.com>
2022-03-29 09:13:56 +0700

    car table

--- src/components/CarTable.vue
+++ src/components/CarTable.vue
@@ -2,10 +2,19 @@
     <n-table :single-line="false" :bordered="true">
         <thead>
             <tr>
+                <th colspan="6">
+                    <n-space justify="end">
+                        <n-button>Add a car</n-button>
+                    </n-space>
+                </th>
+            </tr>
+        </thead>
+        <thead>
+            <tr>
                 <th>Name</th>
                 <th>Type</th>
                 <th>Image</th>
-                <th>manufacturer</th>
+                <th>Manufacturer</th>
                 <th>status</th>
                 <th>Action</th>
             </tr>
@@ -18,20 +27,14 @@
                 <td>{{ item.manufacturer }}</td>
                 <td>{{ item.status }}</td>
                 <td>
-                    <n-button
-                        strong
-                        secondary
-                        circle
-                        type="info"
-                        @click="showModal.status = true, editCar(item.id)"
-                    >
+                    <n-button strong secondary circle type="info" @click="showModal.status = true">
                         <template #icon>
                             <n-icon>
                                 <BuildIcon />
                             </n-icon>
                         </template>
                     </n-button>
-                    <n-button strong secondary circle type="info" @click="deleteUser(item.id)">
+                    <n-button strong secondary circle type="info" @click="deleteCar(item.id)">
                         <template #icon>
                             <n-icon>
                                 <TrashIcon />
@@ -54,6 +57,50 @@
             </tr>
         </tbody>
     </n-table>
+    <n-modal v-model:show="showModal.status">
+        <n-card style="width: 600px" title="Edit user" :bordered="true" size="huge" role="dialog">
+            <template #header-extra>
+                <n-button @click="showModal.status = false" strong secondary circle>
+                    <template #icon>
+                        <n-icon>
+                            <CloseIcon />
+                        </n-icon>
+                    </template>
+                </n-button>
+            </template>
+            <n-grid x-gap="12" y-gap="12" :cols="3">
+                <n-gi>
+                    Name
+                    <n-input placeholder="Name" />
+                </n-gi>
+                <n-gi>
+                    Manufacturer
+                    <n-input clearable placeholder="Manufacturer" />
+                </n-gi>
+                <n-gi>
+                    Type
+                    <n-input clearable placeholder="Type" />
+                </n-gi>
+                <n-gi>
+                    Status
+                    <n-select />
+                </n-gi>
+                <n-gi>
+                    Image
+                    <n-input clearable placeholder="Image" />
+                </n-gi>
+                <n-gi>
+                    <n-space justify="center">
+                        <n-image width="150" src="http://placeimg.com/640/480/cats" />
+                    </n-space>
+                </n-gi>
+            </n-grid>
+            <template #footer>
+                <n-button tertiary @click="showModal.status = false">Cancel</n-button>
+                <n-button tertiary>Save</n-button>
+            </template>
+        </n-card>
+    </n-modal>
 </template>
 
 <script>
@@ -82,14 +129,14 @@ export default {
         this.getCar();
     },
     methods: {
-        async getCar(){
+        async getCar() {
             axios.get(this.carUrl)
-            .then(res => {
-                this.car = JSON.parse(JSON.stringify(res.data))
-            })
-            .catch(err => {
-                console.error(err); 
-            })
+                .then(res => {
+                    this.car = JSON.parse(JSON.stringify(res.data))
+                })
+                .catch(err => {
+                    console.error(err);
+                })
         }
     },
     components: {
